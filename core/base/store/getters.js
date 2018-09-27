926a5c4bdae8999f93f08f8539837dd82db682ca
litao <997963781@qq.com>
2018-09-27 21:46:35 +0800

    代码重构

--- core/base/store/getters.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by litao on 2018/8/10.
- */
-export default {
-    userInfo: state => state.user.userInfo,
-    menuInfo: state => state.menu.menuInfo,
-}
--- src/views/layout/EvLogin.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <span>this is login !</span>
-</template>
