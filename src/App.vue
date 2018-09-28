4dc5cbb7d9ab672e14e12cbc2a42441b54180467
litao <997963781@qq.com>
2018-09-28 17:44:25 +0800

    图标修改

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,8 @@
         <ev-importFile href="//at.alicdn.com/t/font_827121_q3cfy2fwyf.css" type="css"></ev-importFile>
         <ev-importFile href="//at.alicdn.com/t/font_827121_q3cfy2fwyf.js" type="js"></ev-importFile>
 
-        <ev-importFile href="//at.alicdn.com/t/font_853002_vhb0km9l9jd.css" type="css"></ev-importFile>
-        <ev-importFile href="//at.alicdn.com/t/font_853002_vhb0km9l9jd.js" type="js"></ev-importFile>
+        <ev-importFile href="//at.alicdn.com/t/font_853002_wa5wxrqov1.css" type="css"></ev-importFile>
+        <ev-importFile href="//at.alicdn.com/t/font_853002_wa5wxrqov1.js" type="js"></ev-importFile>
 
         <router-view></router-view>
     </div>
